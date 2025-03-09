2c35d82b9f1f967508ae56819d18d7b2b9faa4c8
AndresH1234 <andres.herrerag4@gmail.com>
2025-03-09 15:50:29 -0500

    CorrecionGrafica EcuInvest

--- lib/ui/screens/ecuInvest/grafico_acciones.dart
+++ lib/ui/screens/ecuInvest/grafico_acciones.dart
@@ -3,7 +3,6 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:hackathon_v2_deuna/ui/shared/app_bar_title.dart';
 import 'package:hackathon_v2_deuna/ui/styles/app_colors.dart';
 
-// Nueva pantalla para mostrar el gráfico de fluctuación
 class GraficoAcciones extends StatelessWidget {
   final String nombreEmpresa;
   final List<FlSpot> historialAcciones;
@@ -21,29 +20,39 @@ class GraficoAcciones extends StatelessWidget {
         backgroundColor: AppColors.purple,
         foregroundColor: Colors.white,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: LineChart(
-          LineChartData(
-            gridData: FlGridData(show: true),
-            titlesData: FlTitlesData(show: true),
-            borderData: FlBorderData(show: true),
-            minX: 0,
-            maxX: historialAcciones.length.toDouble() - 1,
-            minY: 0,
-            maxY:
-                historialAcciones
-                    .map((e) => e.y)
-                    .reduce((a, b) => a > b ? a : b) +
-                10, // Establece el máximo y
-            lineBarsData: [
-              LineChartBarData(
-                spots: historialAcciones,
-                isCurved: true,
-                color: Colors.green,
-                belowBarData: BarAreaData(
-                  show: true,
-                  color: Colors.green.withAlpha((255 * 0.5).toInt()),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal, // Permite desplazamiento horizontal
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              SizedBox(
+                width: 400, // Define el ancho del gráfico
+                height: 250, // Define la altura del gráfico
+                child: LineChart(
+                  LineChartData(
+                    gridData: FlGridData(show: true),
+                    titlesData: FlTitlesData(show: true),
+                    borderData: FlBorderData(show: true),
+                    minX: 0,
+                    maxX: historialAcciones.length.toDouble() - 1,
+                    minY: 0,
+                    maxY: historialAcciones
+                            .map((e) => e.y)
+                            .reduce((a, b) => a > b ? a : b) +
+                        10,
+                    lineBarsData: [
+                      LineChartBarData(
+                        spots: historialAcciones,
+                        isCurved: true,
+                        color: Colors.green,
+                        belowBarData: BarAreaData(
+                          show: true,
+                          color: Colors.green.withAlpha((255 * 0.5).toInt()),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
